b30096edcbba7492f675ab6a75330b3cdd1ca2d7
Andrew Maurice <andromulin@gmail.com>
2021-04-20 20:18:06 -0500

    checklist working stretch/nonstretch

--- src/ExerciseDisplay.js
+++ src/ExerciseDisplay.js
@@ -5,6 +5,9 @@ import $ from 'jquery';
 //as exercises are retrieved from the DB, they are placed inside this array. All exercises in this array willl be displayed in a box
 let exercisesOnDisplay = [];
 
+let showStretches = true;
+let showNonStretches = true;
+
 //add all given exercise data to the exercisesOnDisplay arry and trigger the ExerciseDisplay renderer to be updated
 export const SetExercisesOnDisplay = (exercisesToConcat) =>
 {
@@ -61,46 +64,58 @@ export default class ExerciseDisplay extends React.Component
     $(buttonClicked.target).siblings(".contentInBox").toggleClass("hideContentInBox");
   }
 
+  ToggleStretchDisplay = () =>
+  {
+    showStretches = !showStretches;
+    this.forceUpdate();
+  }
+
+  ToggleNonStretchDisplay = () =>
+  {
+    showNonStretches = !showNonStretches;
+    this.forceUpdate();
+  }
 
 
   render(){
     return (
-      <div id="ExerciseBoxContainer">
+      <div>
 
         {/* functionality for the checklist options is inside the exercise box display so it can be used by the display to
         update what content is/is not shown */}
         <div>
           <label>
-            <input type="checkbox" name="Stretches" />
+            <input type="checkbox" name="Stretches" checked={showStretches} onClick={this.ToggleStretchDisplay.bind()}/>
             Stretches
           </label>
 
           <label>
-            <input type="checkbox" name="Stretches" />
+            <input type="checkbox" name="NonStretches" checked={showNonStretches} onClick={this.ToggleNonStretchDisplay.bind()}/>
             Non-Stretches
           </label>
         </div>
 
+        <div id="ExerciseBoxContainer">
+          {exercisesOnDisplay.map(exercise => (
+            //for each exercise, add the exercise box class to keep the display consistent, as well as the class for the specific
+            //primary muscle that is being used to allow for easily removing boxes (may not be needed??????? because may be able to 
+            //simply just remove them form the array and this auto-updates). Key msut be unique for each leemnet in react list,
+            //so the name of the exercise was chosen 
 
-        {exercisesOnDisplay.map(exercise => (
-          //for each exercise, add the exercise box class to keep the display consistent, as well as the class for the specific
-          //primary muscle that is being used to allow for easily removing boxes (may not be needed??????? because may be able to 
-          //simply just remove them form the array and this auto-updates). Key msut be unique for each leemnet in react list,
-          //so the name of the exercise was chosen 
-
-          //exercise should be hidden entirely (given the hideContentInBox class) if it is a stretch and the checkbox for stretches is
-          //deactivated, or same thing for non-stretches
-          <div /*className={`${(exercise.Is_Stretch && ) ? "hideContentInBox" : ""}`*/>
-            <div className={[exercise.Primary_Muscle_Name + "Muscle", "ExerciseBox"].join(' ')} key={exercise.Exercise_Name}>
-              <h1><img src="dumbellFavicon_16x16px.png" alt="dumbell 1"/> {exercise.Exercise_Name} <img src="dumbellFavicon_16x16px.png" alt="dumbell 2"/></h1>
-              <p className="muscleNameInBox">{exercise.Primary_Muscle_Name}</p>
-              
-              <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Description: </button> <div className="contentInBox hideContentInBox">{exercise.Description}</div></p>
-              <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Helpful Tips: </button> <div className="contentInBox hideContentInBox">{exercise.Form_Tips}</div></p>
-              <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Video: </button> <div className="contentInBox hideContentInBox">{exercise.Video}</div></p>
-            </div>
-          </div>  
-        ))}
+            //exercise should be hidden entirely (given the hideContentInBox class) if it is a stretch and the checkbox for stretches is
+            //deactivated, or same thing for non-stretches
+            <div className={`${(exercise.Is_Stretch && !showStretches) || (!exercise.Is_Stretch && !showNonStretches) ? "hideContentInBox" : ""}`}>
+              <div className={[exercise.Primary_Muscle_Name + "Muscle", "ExerciseBox"].join(' ')} key={exercise.Exercise_Name}>
+                <h1><img src="dumbellFavicon_16x16px.png" alt="dumbell 1"/> {exercise.Exercise_Name} <img src="dumbellFavicon_16x16px.png" alt="dumbell 2"/></h1>
+                <p className="muscleNameInBox">{exercise.Primary_Muscle_Name}</p>
+                
+                <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Description: </button> <div className="contentInBox hideContentInBox">{exercise.Description}</div></p>
+                <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Helpful Tips: </button> <div className="contentInBox hideContentInBox">{exercise.Form_Tips}</div></p>
+                <p><button className="contentInBoxTitle" onClick={this.ToggleContentVisibility.bind(this)}>Video: </button> <div className="contentInBox hideContentInBox">{exercise.Video}</div></p>
+              </div>
+            </div>  
+          ))}
+        </div>
       </div>
     );}
 }
