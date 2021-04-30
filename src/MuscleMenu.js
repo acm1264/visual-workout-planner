6834eb8d62b0405072cd89585e24d210c52ccb8a
Andrew Maurice <andromulin@gmail.com>
2021-04-30 14:52:18 -0500

    cleaned button menu css

--- src/MuscleMenu.js
+++ src/MuscleMenu.js
@@ -8,58 +8,60 @@ class MuscleMenu extends MuscleStructure
 {
     render() {
         return (
-            <div>
-                <p> 
-                    Front Muscles:
-                <ButtonGroup>
-                    <Button className="AbsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Abs")}>
-                        Abs
-                    </Button>
-                    <Button className="BicepsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Biceps")}>
-                        Biceps
-                    </Button>
-                    <Button className="ChestMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Chest")}>
-                        Chest
-                    </Button>
-                    <Button className="ForearmMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Forearm")}>
-                        Forearm
-                    </Button>
-                    <Button className="QuadsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Quads")}>
-                        Quads
-                    </Button>
-                </ButtonGroup>
-                </p>
-                <p>
-                    Back Muscles:
-                <ButtonGroup>
-                    <Button className="BackMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Back")}>
-                        Back
-                    </Button>
-                    <Button className="GlutesMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Glutes")}>
-                        Glutes
-                    </Button>
-                    <Button className="HamstringsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Hamstrings")}>
-                        Hamstrings
-                    </Button>
-                    <Button className="TricepsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Triceps")}>
-                        Triceps
-                    </Button>
-                </ButtonGroup>
-                </p>
-                <p>
-                    Both:
-                <ButtonGroup>
-                    <Button className="CalfMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Calf")}>
-                        Calf
-                    </Button>
-                    <Button className="ShouldersMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Shoulders")}>
-                        Shoulders
-                    </Button>
-                    <Button className="HeartMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Heart")}>
-                        Heart
-                    </Button>
-                </ButtonGroup>
-                </p>
+            <div id="buttonHolderHolder">
+                <div id="buttonsHolder">
+                    <p className="buttonSubgroup"> 
+                        Front Muscles:
+                    <ButtonGroup>
+                        <Button className="muscleButton AbsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Abs")}>
+                            Abs
+                        </Button>
+                        <Button className="muscleButton BicepsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Biceps")}>
+                            Biceps
+                        </Button>
+                        <Button className="muscleButton ChestMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Chest")}>
+                            Chest
+                        </Button>
+                        <Button className="muscleButton ForearmMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Forearm")}>
+                            Forearm
+                        </Button>
+                        <Button className="muscleButton QuadsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Quads")}>
+                            Quads
+                        </Button>
+                    </ButtonGroup>
+                    </p>
+                    <p className="buttonSubgroup">
+                        Back Muscles:
+                    <ButtonGroup>
+                        <Button className="muscleButton BackMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Back")}>
+                            Back
+                        </Button>
+                        <Button className="muscleButton GlutesMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Glutes")}>
+                            Glutes
+                        </Button>
+                        <Button className="muscleButton HamstringsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Hamstrings")}>
+                            Hamstrings
+                        </Button>
+                        <Button className="muscleButton TricepsMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Triceps")}>
+                            Triceps
+                        </Button>
+                    </ButtonGroup>
+                    </p>
+                    <p className="buttonSubgroup">
+                        Both:
+                    <ButtonGroup>
+                        <Button className="muscleButton CalfMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Calf")}>
+                            Calf
+                        </Button>
+                        <Button className="muscleButton ShouldersMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Shoulders")}>
+                            Shoulders
+                        </Button>
+                        <Button className="muscleButton HeartMuscle MuscleInactive" onClick={() => this.MuscleOnClick("Heart")}>
+                            Heart
+                        </Button>
+                    </ButtonGroup>
+                    </p>
+                </div>
             </div>
         );
     }
